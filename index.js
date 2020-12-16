5dada29247f22aa5d55f825057b235dba57cd071
Dara O'Sullivan <35735171+daraosullivan@users.noreply.github.com>
2020-12-16 19:06:44 +0000

    Topic 10 - Update Movies Data/Route- Not working

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
 import session from 'express-session';
-import password from './authenticate';
+import passport from './authenticate';
 
 dotenv.config();
 
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();
 }
 
 const errHandler = (err, req, res, next) => {
